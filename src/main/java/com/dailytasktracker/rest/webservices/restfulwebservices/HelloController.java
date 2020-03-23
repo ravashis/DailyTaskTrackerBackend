d0210123914a0fb3e12ae85bc9b78b7daf63ba88
ravashis <rvashishta9@gmail.com>
2020-03-23 20:25:02 +0530

    third heroku commit

--- src/main/java/com/dailytasktracker/rest/webservices/restfulwebservices/HelloController.java
+++ src/main/java/com/dailytasktracker/rest/webservices/restfulwebservices/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins="http://localhost:4200")
-@CrossOrigin(origins="https://bootbackend.herokuapp.com/")
+@CrossOrigin(origins="https://reactfrontforbootback.herokuapp.com/")
 public class HelloController {
 
 	@GetMapping("/")
